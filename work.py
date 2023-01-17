f38b768375a6ce2debf399fd091b7c1385c68013
Alexander Maltsev <aleksander.freelancer@gmail.com>
2023-01-17 09:19:20 +0600

    Edit logic math limit

--- work.py
+++ work.py
@@ -1,4 +1,6 @@
 import json
+
+
 def update_limits(programm_data, path) -> dict:
     'Return updated limits of files in programm_data wich is accepted in arguments'
     # Assign name for file of new day f'{i + 2}': int(next_limit)
@@ -14,15 +16,20 @@ def update_limits(programm_data, path) -> dict:
                     mental_work += int(period['когнитивная_нагрузка'])
                 except KeyError:
                     continue
-            next_limit = int((programm_data['limits_for_files'][f'{i + 1}'] - (work_perform - programm_data['limits_for_files'][f'{i + 1}'])) - mental_work * 0.5)
+            next_limit = int((programm_data['limits_for_files'][f'{i + 1}'] - (
+                work_perform - programm_data['limits_for_files'][f'{i + 1}'])) - mental_work * 0.5)
             if (work_perform <= programm_data['limits_for_files'][f'{i + 1}'] and mental_work < programm_data['limit_mental_work']):
                 next_limit = programm_data['limit']
-            elif(next_limit < 0):
+            elif (next_limit < 0):
                 next_limit = 0
+            if (next_limit < 100):
+                next_limit += 50
             amount = i + 2
             programm_data['limits_for_files'][f'{amount}'] = next_limit
     return programm_data
-def add_activity(activites_data:dict, name, value, period):
+
+
+def add_activity(activites_data: dict, name, value, period):
     'Add activity from dictionary of activites_data.'
     list_1 = []
     list_1.append({name: value})
